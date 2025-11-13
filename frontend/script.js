6d1f583572fc31c943643fe48932671d1453a1d0
llpoa <ll.poa@hotmail.com>
2025-11-13 14:08:49 -0300

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 // URL do backend
-const BACKEND_URL = "https://lrbagagensdanificadas.vercel.app/api/enviar";
+const BACKEND_URL = "lrbagagensdanificadas-1nxv.vercel.app";
 
 // Função para formatar localizador
 function formatarLocalizador(input) {
